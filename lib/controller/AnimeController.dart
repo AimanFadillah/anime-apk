44e6dfc0846e3e3514b89b1184aadc8e6d5b7d7d
AimanFadillah <mlakece914@gmail.com>
2024-09-30 08:20:20 +0700

    add infinite scroll

--- lib/controller/AnimeController.dart
+++ lib/controller/AnimeController.dart
@@ -2,10 +2,12 @@ import 'dart:convert';
 import 'package:animan/model/Anime.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 
 class AnimeController extends GetxController{
   RxList<Anime> listAnime = RxList([]);
-  RxBool isLoading = RxBool(false);
+  RxBool loadingStop = RxBool(false);
+  RxInt page = RxInt(1);
 
   @override
   void onInit () {
@@ -13,19 +15,35 @@ class AnimeController extends GetxController{
     super.onInit();
   }
 
-
   getAnimes () async {
     try{
-      isLoading.value = true;
       listAnime.value = [];
-      final response = await http.get(Uri.parse("https://samehadaku-api-man.vercel.app/anime"));
+      final response = await http.get(Uri.parse("https://samehadaku-api-man.vercel.app/anime?page=$page"));
       for(Map<String,dynamic> anime in jsonDecode(response.body)){
         listAnime.add(Anime.fromJson(anime));
       }
-      isLoading.value = false;
+      page.value = page.value + 1;
     }catch(e){
       print(e);
     }
   }
+
+  getAnimesMore () async {
+    print(page.value);
+    try{
+      final response = await http.get(Uri.parse("https://samehadaku-api-man.vercel.app/anime?page=$page"));
+      final List<dynamic> jsonResponse = jsonDecode(response.body);
+      if(jsonResponse.length == 0){
+        loadingStop.value = true;
+      }
+      for (Map<String, dynamic> anime in jsonResponse) {
+        listAnime.add(Anime.fromJson(anime));
+      }
+      page.value = page.value + 1;
+    }catch(e){
+      print(e);
+    }
+  }
+
 
 }
